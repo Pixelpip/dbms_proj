f720ed6ccbd45b9b8efef2d7b4cd7cb3748552f3
Pixelpip <athulkamathg5@gmail.com>
2024-07-27 19:43:52 +0530

    1

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/contact')
+def contact():
+    return 'Hello, World'
+
+
+if __name__=='__main__':
+    app.run(debug=True)
