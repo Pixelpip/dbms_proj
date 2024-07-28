e1fa28d51e3c7dfc3af1ea49d5fa79e499bba4a5
Pixelpip <athulkamathg5@gmail.com>
2024-07-28 10:46:56 +0530

    1

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+posts=[
+    {
+        'author':'Sridhar',
+        'title':'Post 1',
+        'Content':'First Post',
+        'date_posted':'July 28,2024'
+    },
+    {
+        'author':'Unicourt',
+        'title':'Post 2',
+        'Content':'Second Post',
+        'date_posted':'July 29,2024'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 
 if __name__=='__main__':
